d5a045c7d8ded083a4335889e48fce8a6402338f
Erol Demircan <edemircan@technocon.com>
2013-10-18 10:28:33 +0200

    fixed geohash circle with custom caharacter size init

--- src/main/java/ch/hsr/geohash/queries/GeoHashBoundingBoxQuery.java
+++ src/main/java/ch/hsr/geohash/queries/GeoHashBoundingBoxQuery.java
@@ -1,16 +1,16 @@
 /*
  * Copyright 2010, Silvio Heuberger @ IFS www.ifs.hsr.ch
- *
- * This code is release under the LGPL license.
- * You should have received a copy of the license
- * in the LICENSE file. If you have not, see
+ * 
+ * This code is release under the LGPL license. You should have received a copy of the license in the LICENSE file. If you have not, see
  * http://www.gnu.org/licenses/lgpl-3.0.txt
  */
 package ch.hsr.geohash.queries;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import ch.hsr.geohash.BoundingBox;
 import ch.hsr.geohash.GeoHash;
@@ -18,85 +18,94 @@ import ch.hsr.geohash.WGS84Point;
 import ch.hsr.geohash.util.GeoHashSizeTable;
 
 /**
- * This class returns the hashes covering a certain bounding box. There are
- * either 1,2 or 4 susch hashes, depending on the position of the bounding box
- * on the geohash grid.
+ * This class returns the hashes covering a certain bounding box. There are either 1,2 or 4 susch hashes, depending on the position of the bounding box on the
+ * geohash grid.
  */
 public class GeoHashBoundingBoxQuery implements GeoHashQuery, Serializable {
-	private static final long serialVersionUID = 9223256928940522683L;
-	/* there's not going to be more than 4 hashes. */
-	private List<GeoHash> searchHashes = new ArrayList<GeoHash>(4);
-	/* the combined bounding box of those hashes. */
-	private BoundingBox boundingBox;
+    private static final long serialVersionUID = 9223256928940522683L;
+    /* there's not going to be more than 4 hashes. */
+    private Set<GeoHash>      searchHashes     = new HashSet<GeoHash>();
+    /* the combined bounding box of those hashes. */
+    private BoundingBox       boundingBox;
 
-	public GeoHashBoundingBoxQuery(BoundingBox bbox) {
-		int fittingBits = GeoHashSizeTable.numberOfBitsForOverlappingGeoHash(bbox);
-		WGS84Point center = bbox.getCenterPoint();
-		GeoHash centerHash = GeoHash.withBitPrecision(center.getLatitude(), center.getLongitude(), fittingBits);
+    public GeoHashBoundingBoxQuery(BoundingBox bbox) {
+        int fittingBits = GeoHashSizeTable.numberOfBitsForOverlappingGeoHash(bbox);
+        WGS84Point center = bbox.getCenterPoint();
+        GeoHash centerHash = GeoHash.withBitPrecision(center.getLatitude(), center.getLongitude(), fittingBits);
 
-		if (hashFits(centerHash, bbox)) {
-			addSearchHash(centerHash);
-		} else {
-			expandSearch(centerHash, bbox);
-		}
-	}
+        if (hashFits(centerHash, bbox)) {
+            addSearchHash(centerHash);
+        } else {
+            expandSearch(centerHash, bbox);
+        }
+    }
 
-	private void addSearchHash(GeoHash hash) {
-		if (boundingBox == null) {
-			boundingBox = new BoundingBox(hash.getBoundingBox());
-		} else {
-			boundingBox.expandToInclude(hash.getBoundingBox());
-		}
-		searchHashes.add(hash);
-	}
+    public GeoHashBoundingBoxQuery(BoundingBox bbox, int characterSize) {
+        int fittingBits = characterSize * 5;
+        WGS84Point center = bbox.getCenterPoint();
+        GeoHash centerHash = GeoHash.withBitPrecision(center.getLatitude(), center.getLongitude(), fittingBits);
+        if (hashFits(centerHash, bbox)) {
+            addSearchHash(centerHash);
+        } else {
+            expandSearch(centerHash, bbox);
+        }
+    }
 
-	private void expandSearch(GeoHash centerHash, BoundingBox bbox) {
-		addSearchHash(centerHash);
+    private void addSearchHash(GeoHash hash) {
+        if (boundingBox == null) {
+            boundingBox = new BoundingBox(hash.getBoundingBox());
+        } else {
+            boundingBox.expandToInclude(hash.getBoundingBox());
+        }
+        searchHashes.add(hash);
+    }
 
-		for (GeoHash adjacent : centerHash.getAdjacent()) {
-			BoundingBox adjacentBox = adjacent.getBoundingBox();
-			if (adjacentBox.intersects(bbox) && !searchHashes.contains(adjacent)) {
-				addSearchHash(adjacent);
-			}
-		}
-	}
+    private void expandSearch(GeoHash centerHash, BoundingBox bbox) {
+        addSearchHash(centerHash);
+        for (GeoHash adjacent : centerHash.getAdjacent()) {
+            BoundingBox adjacentBox = adjacent.getBoundingBox();
+            if (adjacentBox.intersects(bbox) && !searchHashes.contains(adjacent)) {
+                addSearchHash(adjacent);
+                expandSearch(adjacent, bbox);
+            }
+        }
+    }
 
-	private boolean hashFits(GeoHash hash, BoundingBox bbox) {
-		return hash.contains(bbox.getUpperLeft()) && hash.contains(bbox.getLowerRight());
-	}
+    private boolean hashFits(GeoHash hash, BoundingBox bbox) {
+        return hash.contains(bbox.getUpperLeft()) && hash.contains(bbox.getLowerRight());
+    }
 
-	@Override
-	public boolean contains(GeoHash hash) {
-		for (GeoHash searchHash : searchHashes) {
-			if (hash.within(searchHash)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    @Override
+    public boolean contains(GeoHash hash) {
+        for (GeoHash searchHash : searchHashes) {
+            if (hash.within(searchHash)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	@Override
-	public boolean contains(WGS84Point point) {
-		return contains(GeoHash.withBitPrecision(point.getLatitude(), point.getLongitude(), 64));
-	}
+    @Override
+    public boolean contains(WGS84Point point) {
+        return contains(GeoHash.withBitPrecision(point.getLatitude(), point.getLongitude(), 64));
+    }
 
-	@Override
-	public List<GeoHash> getSearchHashes() {
-		return searchHashes;
-	}
+    @Override
+    public List<GeoHash> getSearchHashes() {
+        return new ArrayList<GeoHash>(searchHashes);
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder bui = new StringBuilder();
-		for (GeoHash hash : searchHashes) {
-			bui.append(hash).append("\n");
-		}
-		return bui.toString();
-	}
+    @Override
+    public String toString() {
+        StringBuilder bui = new StringBuilder();
+        for (GeoHash hash : searchHashes) {
+            bui.append(hash).append("\n");
+        }
+        return bui.toString();
+    }
 
-	@Override
-	public String getWktBox() {
-		return "BOX(" + boundingBox.getMinLon() + " " + boundingBox.getMinLat() + "," + boundingBox.getMaxLon() + " "
-				+ boundingBox.getMaxLat() + ")";
-	}
+    @Override
+    public String getWktBox() {
+        return "BOX(" + boundingBox.getMinLon() + " " + boundingBox.getMinLat() + "," + boundingBox.getMaxLon() + " " + boundingBox.getMaxLat() + ")";
+    }
 }
